e7615da328eaffaafa972321d4bfe2dc6f81e22a
Dmitriy Ofman <dimaofman@gmail.com>
2012-07-12 16:46:59 +0200

    7 & 8

---
+++ python/7.py
@@ -0,0 +1,24 @@
+# c# Sieve of Eratosthenes
+# Code by David Eppstein, UC Irvine, 28 Feb 2002
+# http://code.activestate.com/recipes/117119/
+
+def gen_primes():
+    D = {}  
+    q = 2  
+
+    while True:
+        if q not in D:
+            yield q        
+            D[q * q] = [q]
+        else:
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+        q += 1
+
+primes = gen_primes()
+for x in xrange(10000):
+	primes.next()
+	
+print primes.next()
+
